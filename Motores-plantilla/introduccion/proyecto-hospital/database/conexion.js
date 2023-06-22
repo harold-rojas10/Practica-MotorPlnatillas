244bcbf4c42cb5a89b03d8b95212b245960bf35c
Harold_Rojas <harold.rojas0510@gmail.com>
2023-06-22 09:42:16 -0500

    Veterinaria

---
+++ Motores-plantilla/introduccion/proyecto-hospital/database/conexion.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '$Adm1n10',//aqui va la contraseña de workbench
+    database: 'hospital',
+    charset: 'utf8mb4' // Establecer el conjunto de caracteres en UTF-8
+});
+
+
+connection.connect((error) => { // Conectar a la base de datos
+    if (error) {
+        console.error('Error al conectar a la base de datos: ', error); // Imprimir mensaje de error en la consola
+    }else{
+        console.log(`Conexion exitosa personas`)
+    }
+});
+
+
+module.exports = { connection }; // Exportar la función para obtener usuarios
